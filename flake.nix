d492bc81d2213d9b678385e0661c5e629643d67d
tuhana <tuhana.cat+git@gmail.com>
2026-04-02 14:38:11 +0300

    Update lockfile, `flake-parts/nixpkgs-lib` follows `nixpkgs`

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     import-tree.url = "github:vic/import-tree";
     den.url = "github:vic/den";
 
