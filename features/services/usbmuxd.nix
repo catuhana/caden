490aecabfacd65765d3083a1466d681a18d54cc4
tuhana <tuhana.cat+git@gmail.com>
2026-05-18 13:38:29 +0300

    Replace `usbmuxd2` with `usbmuxd`

--- features/services/usbmuxd.nix
+++ features/services/usbmuxd.nix
@@ -6,7 +6,6 @@ _: {
         {
           services.usbmuxd = {
             enable = true;
-            package = pkgs.usbmuxd2;
           };
 
           environment.systemPackages = [ pkgs.libimobiledevice ];
