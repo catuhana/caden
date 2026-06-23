cb1bd43662f62f955da78a7a0ca66649e698f398
tuhana <tuhana.cat+git@gmail.com>
2026-06-24 01:35:57 +0300

    Adjust GNOME packages

--- features/desktops/gnome.nix
+++ features/desktops/gnome.nix
@@ -4,10 +4,43 @@
       caden.desktops.common
     ];
 
-    nixos = _: {
+    nixos = { pkgs, ... }: {
       services = {
         desktopManager.gnome.enable = true;
         displayManager.gdm.enable = true;
+
+        gnome = {
+          core-apps.enable = false;
+
+          gnome-browser-connector.enable = false;
+          gnome-initial-setup.enable = false;
+        };
+      };
+
+      environment = {
+        systemPackages = [
+          pkgs.decibels
+          pkgs.gnome-calculator
+          pkgs.gnome-calendar
+          pkgs.gnome-characters
+          pkgs.gnome-tecla
+          pkgs.gnome-text-editor
+          pkgs.gnome-weather
+          pkgs.loupe
+          pkgs.nautilus
+          pkgs.papers
+          pkgs.ptyxis
+          pkgs.resources
+          pkgs.showtime
+        ];
+
+        gnome.excludePackages = [
+          (pkgs.runCommandLocal "nixos-background-info" { } "mkdir $out")
+
+          pkgs.gnome-disk-utility
+          pkgs.gnome-tour
+          pkgs.seahorse
+        ];
       };
     };
   };
